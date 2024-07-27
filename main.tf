28988b50a185dbafeedd37f53332904b30c0be1d
Harish divadari <21MH1A0587@acoe.edu.in>
2024-07-27 12:20:33 +0530

    first

---
+++ main.tf
@@ -0,0 +1,44 @@
+#s3 buckcet 
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucketname
+
+
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+    }
+  }
+}
